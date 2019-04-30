d7211d77b4f92112fdd81a9d82ea159bd50f6044
Valery Sedletski <_valerius@mail.ru>
2019-05-01 02:00:47 +0300

    Return back the modifications of base-foc required for ports-foc (l4linux/l4android).

--- repos/base-foc/src/core/include/native_cpu_component.h
+++ repos/base-foc/src/core/include/native_cpu_component.h
@@ -38,8 +38,10 @@ class Genode::Native_cpu_component : public Rpc_object<Foc_native_cpu,
 		Native_cpu_component(Cpu_session_component &, char const *);
 		~Native_cpu_component();
 
+		void enable_vcpu(Thread_capability, addr_t) override;
 		Native_capability native_cap(Thread_capability) override;
 		Foc_thread_state thread_state(Thread_capability) override;
+		Native_capability alloc_irq() override;
 };
 
 #endif /* _CORE__INCLUDE__NATIVE_CPU_COMPONENT_H_ */
--- repos/base-foc/src/core/native_cpu_component.cc
+++ repos/base-foc/src/core/native_cpu_component.cc
@@ -17,6 +17,7 @@
 /* core includes */
 #include <native_cpu_component.h>
 #include <cpu_session_component.h>
+#include <cpu_session_irqs.h>
 #include <platform.h>
 
 /* Fiasco.OC includes */
@@ -25,6 +26,25 @@ namespace Fiasco {
 #include <l4/sys/factory.h>
 }
 
+static Genode::Avl_tree<Genode::Cpu_session_irqs> _irq_tree;
+
+void Genode::Native_cpu_component::enable_vcpu(Genode::Thread_capability thread_cap,
+                                                Genode::addr_t vcpu_state)
+{
+	using namespace Genode;
+	using namespace Fiasco;
+
+	auto lambda = [&] (Cpu_thread_component *thread) {
+		if (!thread) return;
+
+		l4_cap_idx_t tid = thread->platform_thread().thread().local.data()->kcap();
+
+		l4_msgtag_t tag = l4_thread_vcpu_control(tid, vcpu_state);
+		if (l4_msgtag_has_error(tag))
+			warning("l4_thread_vcpu_control failed");
+	};
+	_thread_ep.apply(thread_cap, lambda);
+}
 
 Genode::Native_capability
 Genode::Native_cpu_component::native_cap(Genode::Thread_capability cap)
@@ -51,6 +71,37 @@ Genode::Native_cpu_component::thread_state(Genode::Thread_capability cap)
 	return _thread_ep.apply(cap, lambda);
 }
 
+
+Genode::Native_capability Genode::Native_cpu_component::alloc_irq()
+{
+	using namespace Fiasco;
+	using namespace Genode;
+
+	/* find irq object container of this cpu-session */
+	Cpu_session_irqs* node = _irq_tree.first();
+	if (node)
+		node = node->find_by_session(&_cpu_session);
+
+	/* if not found, we've to create one */
+	if (!node) {
+		node = new (&_cpu_session._md_alloc) Cpu_session_irqs(&_cpu_session);
+		_irq_tree.insert(node);
+	}
+
+	/* construct irq kernel-object */
+	Cap_index* i = cap_map().insert(platform_specific().cap_id_alloc().alloc());
+	//Cap_index* i = cap_map()->insert(platform_specific()->cap_id_alloc()->alloc());
+	l4_msgtag_t res = l4_factory_create_irq(L4_BASE_FACTORY_CAP, i->kcap());
+	if (l4_error(res)) {
+		warning("Allocation of irq object failed!");
+		return Genode::Native_capability();
+	}
+
+	/* construct cap and hold a reference in the irq container object */
+	Genode::Native_capability cap(i);
+	//Genode::Native_capability cap(*i);
+	return (node->add(cap)) ? cap : Genode::Native_capability();
+}
 
 Genode::Native_cpu_component::Native_cpu_component(Cpu_session_component &cpu_session, char const *)
 :
